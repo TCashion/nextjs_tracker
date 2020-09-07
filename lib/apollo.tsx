23bbef8717acbf3a6f18487f6f1bffce4806c2c4
TCashion <58757351+TCashion@users.noreply.github.com>
2020-09-07 17:25:39 -0600

    beef up apollo.tsx

--- lib/apollo.tsx
+++ lib/apollo.tsx
@@ -1,24 +1,88 @@
 import ApolloClient, { NormalizedCacheObject } from 'apollo-boost';
+import Head from 'next/Head';
 import { ApolloProvider } from '@apollo/react-hooks';
 import fetch from 'isomorphic-unfetch';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+// change to this later to resolve typescript error. 
+// import {
+//     ApolloClient,
+//     InMemoryCache,
+//     NormalizedCacheObject,
+// } from '@apollo/client'
 
+export function withApollo (PageComponent: any) {
+    const WithApollo = ({ apolloClient, apolloState, ...pageProps }: any) => {
 
-export function withApollo (PageComponent: () => JSX.Element) {
-    const WithApollo = <T extends object>(props: T) => {
-
-        let client: ApolloClient<NormalizedCacheObject> | undefined;
-        client = new ApolloClient({
-            uri: 'http://localhost:3000/api/graphql',
-            fetch
-        });
+        const client: any = apolloClient || initApolloClient();
 
         return (
             <ApolloProvider
                 client={client}
             >
-                <PageComponent {...props} />
+                <PageComponent {...pageProps} />
             </ApolloProvider>
         )
     }
+
+    // allows us to set the initial props, with context (res, req) passed along
+    WithApollo.getInitialProps = async (ctx: any) => {
+        // this is the entire tree of our application
+        const { AppTree } = ctx;
+        const apolloClient = ( ctx.apolloClient = initApolloClient() );
+
+        let pageProps = {};
+        if (PageComponent.getInitialProps) {
+            pageProps = await PageComponent.getInitialProps(ctx);
+        };
+
+        // if query is on the server
+        if (typeof window === 'undefined') {
+            if ( ctx.res && ctx.res.finished ) {
+                return pageProps;
+            }
+
+            try {
+                // gets all data from the tree before application is rendered
+                const { getDataFromTree } = await import('@apollo/react-ssr');
+                await getDataFromTree(
+                    // this instructs to render the whole tree, but only after
+                    // adding the apollo data to the tree
+                    <AppTree 
+                        pageProps={{
+                            ...pageProps,
+                            apolloClient
+                        }}
+                    />
+                )
+            } catch (e) {
+                console.error(e);
+            }
+
+            // manually clears a Head sideffect 
+            Head.rewind(); 
+        }
+        
+        const apolloState = apolloClient.cache.extract();
+        return {
+            ... pageProps,
+            apolloState
+        }
+    };
+
+
     return WithApollo;
 };
+
+const initApolloClient = (initialState = {}) => {
+    const ssrMode = typeof window === 'undefined';
+    const cache = new InMemoryCache().restore(initialState); 
+
+
+    const client = new ApolloClient({
+        ssrMode,
+        uri: 'http://localhost:3000/api/graphql',
+        fetch, 
+        cache,
+    });
+    return client;
+}
