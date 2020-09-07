4f4e8a92d1ac7f6cabc90bd1a82ef4512a814692
TCashion <58757351+TCashion@users.noreply.github.com>
2020-09-06 18:50:24 -0600

    configure tailwindcss

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,23 +1,27 @@
 import Link from 'next/link';
+import { link } from '../types/interfaces';
+
+
+const links: link[] = [
+    { label: 'Home', href: '/' },
+    { label: 'About', href: '/about' },
+]
 
 const Nav = () => {
     return (
-        <header>        
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/">
-                            <a>Home</a>
-                        </Link>
-                    </li>
-                    <li>
-                        |
-                    </li>
-                    <li>
-                        <Link href="/about">
-                            <a>About</a>
-                        </Link>
-                    </li>
+        <header>
+            <nav className="flex justify-center">
+                <ul className="flex">
+                    {links.map((link, idx) => (
+                        <>
+                            <li className="pr-8 pl-8">
+                                <Link href={link.href}>
+                                    <a>{link.label}</a>
+                                </Link>
+                            </li>
+                            {idx !== links.length - 1 && '|'}
+                        </>
+                    ))}
                 </ul>
             </nav>
         </header>
---
+++ types/interfaces.ts
@@ -0,0 +1,4 @@
+export interface link {
+    label: string,
+    href: string
+}
