cc2cb5640216ae6d0fac3a353e308b1cac111cdd
TCashion <58757351+TCashion@users.noreply.github.com>
2020-09-07 16:21:53 -0600

    solved pagecomponent typescript error

--- lib/apollo.tsx
+++ lib/apollo.tsx
@@ -1,16 +1,13 @@
-import ApolloClient from 'apollo-boost';
+import ApolloClient, { NormalizedCacheObject } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 import fetch from 'isomorphic-unfetch';
 
-type PageComponent = {
-    props: any,
-    children?: ReactNode
-}
 
-export function withApollo (PageComponent: PageComponent) {
+export function withApollo (PageComponent: () => JSX.Element) {
     const WithApollo = <T extends object>(props: T) => {
 
-        const client = new ApolloClient({
+        let client: ApolloClient<NormalizedCacheObject> | undefined;
+        client = new ApolloClient({
             uri: 'http://localhost:3000/api/graphql',
             fetch
         });
