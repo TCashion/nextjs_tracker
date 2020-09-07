6023bbfcfea4936fef645ff0b3e2c69aab012750
TCashion <58757351+TCashion@users.noreply.github.com>
2020-09-06 18:06:50 -0600

    rename components to lowercase

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import styles from '../styles/Home.module.css'
-import Layout from '../components/Layout'
-import Nav from '../components/Nav'
+import Layout from '../components/layout'
+import Nav from '../components/nav'
 
 export default function Home() {
   return (
